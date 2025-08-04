0fbc86f52a47d179afed318c657114ec91427bd4
Juergen Bohl <juergen.bohl@zeiss.com>
2025-08-04 12:34:05 +0200

    add a function call which crashes (with the embedded jxrlib library)

--- tests/jxrlib_coexistence/main.cpp
+++ tests/jxrlib_coexistence/main.cpp
@@ -3,11 +3,11 @@
 #include <libCZI/libCZI.h>
 #include <jxrlib/JXRGlue.h>
 
-int main() 
+int main()
 {
 	PKFactory* pFactory = nullptr;
 	ERR err = PKCreateFactory(&pFactory, 0);
-	if (err != WMP_errSuccess) 
+	if (err != WMP_errSuccess)
 	{
 		std::cerr << "Failed to create PKFactory: " << err << std::endl;
 		return EXIT_FAILURE;
@@ -16,19 +16,20 @@ int main()
 	std::uint8_t stream_data[32] = { 0 };
 	WMPStream* pStream = nullptr;
 	err = pFactory->CreateStreamFromMemory(&pStream, stream_data, sizeof(stream_data));
-	if (err != WMP_errSuccess) 
+	if (err != WMP_errSuccess)
 	{
 		std::cerr << "Failed to create WMPStream: " << err << std::endl;
 		return EXIT_FAILURE;
 	}
 
+	pStream->Close(&pStream);
 	pFactory->Release(&pFactory);
 
 	std::cout << "WMPStream created successfully." << std::endl;
 
 	PKImageEncode* pIE = nullptr;
 	err = PKImageEncode_Create_WMP(&pIE);
-	if (err != WMP_errSuccess) 
+	if (err != WMP_errSuccess)
 	{
 		std::cerr << "Failed to create PKImageEncode: " << err << std::endl;
 		return EXIT_FAILURE;
@@ -38,7 +39,7 @@ int main()
 
 	std::cout << "PKImageEncode created successfully." << std::endl;
 
-	int major, minor, patch,tweak;
+	int major, minor, patch, tweak;
 	libCZI::GetLibCZIVersion(&major, &minor, &patch, &tweak);
 	std::cout << "LibCZI Version: " << major << "." << minor << "." << patch << " tweak: " << tweak << std::endl;
 	std::cout << std::endl;
@@ -51,6 +52,6 @@ int main()
 	std::cout << "repository: " << build_information.repositoryUrl << std::endl;
 	std::cout << "branch: " << build_information.repositoryBranch << std::endl;
 	std::cout << "tag: " << build_information.repositoryTag << std::endl;
-    
+
 	return EXIT_SUCCESS;
 }
