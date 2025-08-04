994dc816079a519901aff1c7f2dff69354733277
Juergen Bohl <juergen.bohl@zeiss.com>
2025-08-04 12:29:33 +0200

    Add jxrlib-coexistence test application and CMake setup
    Updated `build.yml` to include steps for building the
    `jxrlib-coexistence` test application using `vcpkg` for
    both Windows and Linux. Modified `CMakeLists.txt` to
    specify the minimum CMake version, integrate `vcpkg`,
    and define the project structure. Added `main.cpp`
    demonstrating usage of `libCZI` and `jxrlib` libraries,
    including factory creation and stream handling.

---
+++ tests/jxrlib_coexistence/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstdint>
+#include <libCZI/libCZI.h>
+#include <jxrlib/JXRGlue.h>
+
+int main() 
+{
+	PKFactory* pFactory = nullptr;
+	ERR err = PKCreateFactory(&pFactory, 0);
+	if (err != WMP_errSuccess) 
+	{
+		std::cerr << "Failed to create PKFactory: " << err << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::uint8_t stream_data[32] = { 0 };
+	WMPStream* pStream = nullptr;
+	err = pFactory->CreateStreamFromMemory(&pStream, stream_data, sizeof(stream_data));
+	if (err != WMP_errSuccess) 
+	{
+		std::cerr << "Failed to create WMPStream: " << err << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	pFactory->Release(&pFactory);
+
+	std::cout << "WMPStream created successfully." << std::endl;
+
+	PKImageEncode* pIE = nullptr;
+	err = PKImageEncode_Create_WMP(&pIE);
+	if (err != WMP_errSuccess) 
+	{
+		std::cerr << "Failed to create PKImageEncode: " << err << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	pIE->Release(&pIE);
+
+	std::cout << "PKImageEncode created successfully." << std::endl;
+
+	int major, minor, patch,tweak;
+	libCZI::GetLibCZIVersion(&major, &minor, &patch, &tweak);
+	std::cout << "LibCZI Version: " << major << "." << minor << "." << patch << " tweak: " << tweak << std::endl;
+	std::cout << std::endl;
+
+	libCZI::BuildInformation build_information;
+	libCZI::GetLibCZIBuildInformation(build_information);
+	std::cout << "LibCZI Build Information:" << std::endl;
+	std::cout << "-------------------------" << std::endl;
+	std::cout << "compiler: " << build_information.compilerIdentification << std::endl;
+	std::cout << "repository: " << build_information.repositoryUrl << std::endl;
+	std::cout << "branch: " << build_information.repositoryBranch << std::endl;
+	std::cout << "tag: " << build_information.repositoryTag << std::endl;
+    
+	return EXIT_SUCCESS;
+}
