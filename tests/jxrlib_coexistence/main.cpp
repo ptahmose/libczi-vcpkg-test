c985fd6a946f50e8ae55c2c7717093e2047090df
ptahmose <jbohl@h-quer.de>
2025-08-04 17:33:05 +0200

    typos

--- tests/jxrlib_coexistence/main.cpp
+++ tests/jxrlib_coexistence/main.cpp
@@ -4,7 +4,7 @@
 #include <libCZI/libCZI.h>
 
 // Some trickery in needed (on Linux) to successfully include JXRGlue.h - "FAR" is used there, but this is not
-//  recognized by most compilers. Also beware, that macros for min/max are defined, and maybe are more problematic macros.
+//  recognized by most compilers. Also be aware that macros for min/max are defined, and maybe there are more problematic macros.
 #pragma push_macro("FAR")
 #define FAR
 #include <jxrlib/JXRGlue.h>
